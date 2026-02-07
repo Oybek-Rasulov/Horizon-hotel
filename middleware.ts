3673bd6e141184013554a8274e542dea5870c9de
Oybek <oybekrasulov2222@gmail.com>
2026-02-07 11:46:54 +0500

    roomsid protected

--- middleware.ts
+++ middleware.ts
@@ -11,5 +11,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/reservations/:path*", "/rooms/:path*"],
+  matcher: ["/reservations/:path*", "/rooms/:id"],
 };
